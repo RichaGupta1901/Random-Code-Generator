35fb0212937e17e833d4d608599a4cd7ee80cb62
Thomas Ryste <thomas.ryste@gmail.com>
2025-03-04 10:17:31 +0100

    Update RandomText.py
    added special characters

--- RandomText.py
+++ RandomText.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_code(length=6):
-    characters = string.ascii_letters + string.digits  # A-Z, a-z, 0-9
+    characters = string.ascii_letters + string.digits  + string.punctiation # A-Z, a-z, 0-9, special characters
     return ''.join(random.choices(characters, k=length))
 
 # Example usage
